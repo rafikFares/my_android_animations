0165e8a33d90c96be7f187dc84e9248521d908f6
rafikFares <rafik.f03@gmail.com>
2019-11-18 16:47:47 +0100

    patch animations using vector drawableé

--- app/src/main/java/com/example/myanimations/MainAnimationVectorDrawable.kt
+++ app/src/main/java/com/example/myanimations/MainAnimationVectorDrawable.kt
@@ -1,15 +1,29 @@
 package com.example.myanimations
 
+import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import kotlinx.android.synthetic.main.animation_vector_drawable_main.*
 
 class MainAnimationVectorDrawable : AppCompatActivity() {
 
+    private var rocketAnimation: AnimatedVectorDrawableCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.animation_vector_drawable_main)
 
+        rocketAnimation = AnimatedVectorDrawableCompat.create(this, R.drawable.animator_vector_drawable)
+
+        imageView4.apply {
+            setImageDrawable(rocketAnimation)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+         rocketAnimation?.start() // this really need to be here
     }
 
 }
