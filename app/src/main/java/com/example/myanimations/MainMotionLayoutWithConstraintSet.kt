b45c0e74b31ef639719eca7b9bc2540429d7bc4f
rafikFares <rafik.f03@gmail.com>
2019-11-20 10:43:06 +0100

    add motion layout with 1 layout xml and 1 xml scene, with constraintSet with constraints and custom attribute and key attribute

---
+++ app/src/main/java/com/example/myanimations/MainMotionLayoutWithConstraintSet.kt
@@ -0,0 +1,12 @@
+package com.example.myanimations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainMotionLayoutWithConstraintSet : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.motion_layout_for_constraint_set)
+    }
+}
